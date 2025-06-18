a2dee86d7c679b41c726e1970c8cd7105d8f77e4
kepha-wiz <kephacheps55@gmail.com>
2025-06-18 06:36:58 -0700

    Delete debate directory

--- debate/js/script.js
+++
@@ -1,9 +0,0 @@
-
-const currentPage = location.pathname.split("/").slice(-1)[0];
-const navLinks = document.querySelectorAll("nav a");
-
-navLinks.forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
-});
