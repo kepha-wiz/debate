84ed40e0ab490f9c51d24082dc8ebf8ceb03a981
kepha-wiz <kephacheps55@gmail.com>
2025-06-18 06:32:08 -0700

    Add files via upload

---
+++ debate/js/script.js
@@ -0,0 +1,9 @@
+
+const currentPage = location.pathname.split("/").slice(-1)[0];
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
